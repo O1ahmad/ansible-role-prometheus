9eac0642240441aa74fbe19bcb03bee12c97f9ad
Ahmad Wilson <ahwilson712@gmail.com>
2020-01-03 18:08:27 -0500

    add alertmanager install, config and launch functionality

---
+++ test/integration/launch/alertmanager_launch/default_test.rb
@@ -0,0 +1,15 @@
+title "Prometheus service launch test suite"
+
+describe file('/etc/systemd/system/alertmanager.service') do
+  it { should exist }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+  its('mode') { should cmp '0644' }
+
+end
+
+describe service('alertmanager') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
