18e3bf00abfb5af04687833d0541e594884122d6
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-02-10 19:38:50 -0500

    update supported minimum Ansible version and apply refactorizations

--- test/integration/setup/setup_exporters/default_test.rb
+++ test/integration/setup/setup_exporters/default_test.rb
@@ -4,14 +4,12 @@ describe directory('/opt/prometheus/exporters/node_exporter') do
   it { should exist }
   its('owner') { should eq 'prometheus' }
   its('group') { should eq 'prometheus' }
-  its('mode') { should cmp '0775' }
 end
 
 describe file('/etc/systemd/system/node_exporter.service') do
   it { should exist }
   its('owner') { should eq 'root' }
   its('group') { should eq 'root' }
-  its('mode') { should cmp '0644' }
 
   its('content') { should match('--log.level=debug') }
   its('content') { should match('--web.listen-address=') }
